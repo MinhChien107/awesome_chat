07ff5e81756bef8837c76b23bb7f158c148569e3
MinhChien107 <dangminhchien107@gmail.com>
2022-02-16 16:16:40 +0700

    [1.0.0] Add static pages

--- src/server.js
+++ src/server.js
@@ -1,25 +1,22 @@
 import express from "express";
-import { async } from "regenerator-runtime";
 import ConnectDB from "./config/connectDB";
-import ContactModel from './models/contact.model'
-
+import configViewEngine from "./config/viewEngine"
 const app = express();
 
 //Connect to mongoDB 
 ConnectDB();
 
-app.get("/test-database", async(req, res)=>{
-  try {
-    let item = {
-      userId: "123151",
-      contactId: "kfasdf"
-    }
-    let contact = await ContactModel.createNew(item);
-    res.send(contact)
-  } catch (error) {
-    console.log(error)
-  }
+//config view engine
+configViewEngine(app)
+
+app.get("/", async(req, res)=>{
+  res.render("main/master")
 });
+
+app.get("/login-register", async(req, res)=>{
+  res.render("auth/loginRegister")
+});
+
 
 app.listen(process.env.APP_PORT, process.env.APP_HOST, ()=> {
   console.log(`Running at ${process.env.APP_HOST}:${process.env.APP_PORT}`)
